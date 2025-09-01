89793ac84589e94a1804381b7bd785f883a6fa56
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2025-09-01 13:05:12 -0300

    feat: create configuration service and validation for enviroment variables

---
+++ src/core/config/enviroment.ts
@@ -0,0 +1,57 @@
+import { Transform } from 'class-transformer';
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export default class EnvironmentVariables {
+  @IsEnum(['dev', 'tst', 'prd'])
+  @IsString()
+  NODE_ENV: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_HOST: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  DATABASE_PORT: number;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_USERNAME: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_PASSWORD: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_NAME: string;
+
+  @Transform(({ value }: { value: string }) =>
+    typeof value === 'string' ? parseInt(value, 10) : value,
+  )
+  @IsNotEmpty()
+  @IsNumber()
+  SALT: number;
+
+  @IsNotEmpty()
+  @IsString()
+  JWT_SECRET: string;
+
+  @Transform(({ value }: { value: string }) =>
+    typeof value === 'string' ? parseInt(value, 10) : value,
+  )
+  @IsNumber()
+  @IsNotEmpty()
+  PORT: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  ACCESS_TOKEN_EXPIRES_IN_SECONDS: number;
+  @IsNumber()
+  @IsNotEmpty()
+  REFRESH_TOKEN_EXPIRES_IN_SECONDS: number;
+
+
+
+  static
+}
