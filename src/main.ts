4e079c528518f9954bc3e3991f64070c8961ab80
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2025-09-01 12:26:46 -0300

    feat: configurate swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Base APi')
+    .setDescription('Base Api')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'DEV')
+    .addTag('Base Api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
