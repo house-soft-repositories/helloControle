c25606d990fe1e9da3a774dc840a171dc419c015
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2025-09-02 08:30:36 -0300

    feat: configure typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
+import CoreModule from '@/core/core_module';
+import ConfigurationService from '@/core/services/configuration.service';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [CoreModule],
+      inject: [ConfigurationService],
+      useFactory: (configurationService: ConfigurationService) => ({
+        type: 'postgres',
+        host: configurationService.get('DATABASE_HOST'),
+        port: configurationService.get('DATABASE_PORT'),
+        username: configurationService.get('DATABASE_USERNAME'),
+        password: configurationService.get('DATABASE_PASSWORD'),
+        database: configurationService.get('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.model{.ts,.js}'],
+        logging: ['error'],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
