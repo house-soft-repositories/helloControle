90dd14b17d91697c7fd6f413e966eb41ae96e134
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2025-09-02 08:31:46 -0300

    feat: create default messages for system

--- src/core/constants/error_messages.ts
+++ src/core/constants/error_messages.ts
@@ -1,3 +1,5 @@
 export default abstract class ErrorMessages {
   static readonly UNEXPECTED_ERROR = 'Unexpected error occurred';
+  static readonly USER_NOT_FOUND = 'User not found';
+  static readonly USER_ALREADY_EXISTS = 'E-mail or password not valid';
 }
