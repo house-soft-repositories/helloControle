a30cb771f627c4a2c8e50010ff0ecf99b410f1e4
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2025-09-02 08:31:35 -0300

    feat: create use case pattern

--- src/core/interface/use_case.ts
+++ src/core/interface/use_case.ts
@@ -1,6 +1,6 @@
 import AppException from '@/core/exceptions/app_exception';
 import AsyncResult from '@/core/types/async_result';
 
-export default interface UseCase<P, E extends AppException, R> {
-  execute(param: P): AsyncResult<E, R>;
+export default interface UseCase<P, R> {
+  execute(param: P): AsyncResult<AppException, R>;
 }
