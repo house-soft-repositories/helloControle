30742e2a39db85a5e1bd4c619fceb102ce98b267
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2025-09-05 13:23:37 -0300

    feat: adjust contract contract repository extends base repository

--- src/modules/contract/adapters/i_contract_repository.ts
+++ src/modules/contract/adapters/i_contract_repository.ts
@@ -1,26 +1,23 @@
 import AsyncResult from '@/core/types/async_result';
+import { Unit } from '@/core/types/unit';
 import ContractEntity from '@/modules/contract/domain/entities/contract.entity';
 import ContractRepositoryException from '@/modules/contract/exceptions/contract_repository_exception';
+import { ContractQueryOptions } from '@/modules/contract/infra/query/query_objects';
 
 export default interface IContractRepository {
   save(
     contract: ContractEntity,
   ): AsyncResult<ContractRepositoryException, ContractEntity>;
+  findOne(
+    query: ContractQueryOptions,
+  ): AsyncResult<ContractRepositoryException, ContractEntity>;
   findAll(): AsyncResult<ContractRepositoryException, ContractEntity[]>;
-  findById(
-    id: string,
-  ): AsyncResult<ContractRepositoryException, ContractEntity | null>;
-  findByUuid(
-    uuid: string,
-  ): AsyncResult<ContractRepositoryException, ContractEntity | null>;
+
   findByOrgao(
     orgao: string,
   ): AsyncResult<ContractRepositoryException, ContractEntity[]>;
   findByEmpresa(
     empresa: string,
   ): AsyncResult<ContractRepositoryException, ContractEntity[]>;
-  update(
-    contract: ContractEntity,
-  ): AsyncResult<ContractRepositoryException, ContractEntity>;
-  delete(id: string): AsyncResult<ContractRepositoryException, void>;
+  delete(id: string): AsyncResult<ContractRepositoryException, Unit>;
 }
