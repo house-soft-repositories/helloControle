3198d59e41eedc0382a8974515ad8f0b428c8255
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2025-09-05 13:21:20 -0300

    feat: adjust interface for service exception

--- src/core/exceptions/service.exception.ts
+++ src/core/exceptions/service.exception.ts
@@ -1,7 +1,7 @@
 import AppException from '@/core/exceptions/app_exception';
 
 export default class ServiceException extends AppException {
-  constructor(message: string, statusCode: number = 400) {
-    super(message, statusCode);
+  constructor(message: string, statusCode: number = 400, cause?: Error) {
+    super(message, statusCode, cause);
   }
 }
