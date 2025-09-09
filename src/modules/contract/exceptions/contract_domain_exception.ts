61af8fcafe4c3ce2b64873a7c527c015d994cd16
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2025-09-09 08:30:45 -0300

    fix: adjust contract domain exception extends app exception not error

--- src/modules/contract/exceptions/contract_domain_exception.ts
+++ src/modules/contract/exceptions/contract_domain_exception.ts
@@ -1,6 +1,7 @@
-export default class ContractDomainException extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'ContractDomainException';
+import AppException from '@/core/exceptions/app_exception';
+
+export default class ContractDomainException extends AppException {
+  constructor(message: string, statusCode = 400, error?: Error) {
+    super(message, statusCode, error);
   }
 }
